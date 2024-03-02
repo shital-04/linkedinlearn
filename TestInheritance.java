f1b07f60f998080a94335bbb474e29ec519b4945
Shital N <shitaln@test.com>
2024-03-02 14:44:39 +0530

    Removed the blank table from bottom of the page

---
+++ TestInheritance.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class TestInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Manager M1=new Manager();
+	M1.setInfo(101,"Raj",50000,"Engineer");
+	M1.DisplayDetails();
+	M1.setBonus(100000);
+	M1.doAppraisals();
+	M1.doReviews();
+	
+	
+	}
+
+}
